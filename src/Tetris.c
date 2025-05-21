4c5762173ba3433a2abfbc5f2d57b848a9e1bcd4
Valentin Jonckheere <valentin@jonckheere.fr>
2025-05-21 02:14:55 -0400

    Added support for Pebble Time Round

--- src/Tetris.c
+++ src/Tetris.c
@@ -5,6 +5,32 @@
 
 #include "helpers.h"
 
+#define BLOCK_SIZE 8 
+#define GRID_BLOCK_WIDTH 10
+#define GRID_BLOCK_HEIGHT 20
+
+#if defined(PBL_RECT)
+  #define GRID_PIXEL_WIDTH (BLOCK_SIZE * GRID_BLOCK_WIDTH)
+  #define GRID_PIXEL_HEIGHT (BLOCK_SIZE * GRID_BLOCK_HEIGHT)
+
+  #define GRID_ORIGIN_X 0
+  #define GRID_ORIGIN_Y 0
+#endif
+
+#if defined(PBL_PLATFORM_CHALK)
+  #define SCREEN_WIDTH 180
+  #define SCREEN_HEIGHT 180
+
+  #define GRID_PIXEL_WIDTH (BLOCK_SIZE * GRID_BLOCK_WIDTH)
+  #define GRID_PIXEL_HEIGHT (BLOCK_SIZE * GRID_BLOCK_HEIGHT)
+
+  #define GRID_ORIGIN_X ((SCREEN_WIDTH - GRID_PIXEL_WIDTH) / 2)
+  #define GRID_ORIGIN_Y ((SCREEN_HEIGHT - GRID_PIXEL_HEIGHT) / 2)
+#endif
+
+#define LABEL_HEIGHT 20 
+
+
 // arbitrary persistant storage keys.
 const uint32_t SCORE_KEY = 1234;
 const uint32_t GRID_KEY = 5678;
@@ -33,8 +59,8 @@ static TextLayer *option_shadows_layer;
 static TextLayer *high_score_layer;
 static GPath *selector_path;
 
-static uint8_t grid[10][20];
-static uint8_t grid_col[10][20];
+static uint8_t grid[GRID_BLOCK_WIDTH][GRID_BLOCK_HEIGHT];
+static uint8_t grid_col[GRID_BLOCK_WIDTH][GRID_BLOCK_HEIGHT];
 static GColor col_map[7];
 // Normally we can't rely on stack-allocated GPoints.
 // But these do not need to retain their values for long,
@@ -67,12 +93,16 @@ static char levelStr[10];
 static char high_score_buffer[10];
 static char high_score_str[22];
 static Layer *s_bg_layer = NULL;
-static Layer *s_left_pane_layer = NULL;
+static Layer *s_game_pane_layer = NULL;
 static Layer *s_title_pane_layer = NULL;
 
 static GColor blueish;
 static GColor gray;
 static GColor purple;
+
+// ------------------------ //
+// **** GAME FUNCTIONS **** //
+// ------------------------ //
 
 static void drop_block() {
   if (blockType == -1) {
@@ -149,13 +179,14 @@ static void drop_block() {
           lost = true;
           Layer *window_layer = window_get_root_layer(window);
           layer_remove_from_parent(s_bg_layer);
-          layer_remove_from_parent(s_left_pane_layer);
+          layer_remove_from_parent(s_game_pane_layer);
           layer_add_child(window_layer, text_layer_get_layer(title_layer));
           text_layer_set_text(title_layer, "You lost!");
           text_layer_set_text(score_label_layer, "");
           text_layer_set_text(score_layer, "");
           text_layer_set_text(level_label_layer, "");
           text_layer_set_text(level_layer, "");
+
           // When you lose, wipe the save.
           // No need to get fancy; just mark 'has save' false since
           // we'll re-create the whole thing when it gets set to true.
@@ -173,7 +204,7 @@ static void drop_block() {
     }
   }
 
-  layer_mark_dirty(s_left_pane_layer);
+  layer_mark_dirty(s_game_pane_layer);
 }
 
 static void game_tick(void *data) {
@@ -187,7 +218,7 @@ static void setup_game() {
     playing = true;
     Layer *window_layer = window_get_root_layer(window);
     layer_add_child(window_layer, s_bg_layer);
-    layer_add_child(window_layer, s_left_pane_layer);
+    layer_add_child(window_layer, s_game_pane_layer);
     layer_remove_from_parent(s_title_pane_layer);
     layer_remove_from_parent(text_layer_get_layer(new_game_label_layer));
     layer_remove_from_parent(text_layer_get_layer(load_game_label_layer));
@@ -209,7 +240,7 @@ static void setup_game() {
     nextBlockType = rand() % 7;
     drop_block();
     layer_mark_dirty(s_bg_layer);
-    layer_mark_dirty(s_left_pane_layer);
+    layer_mark_dirty(s_game_pane_layer);
 }
 
 // TODO This should probably go in the helper file with the save logic.
@@ -257,7 +288,7 @@ static void restart_after_loss() {
   load_choice = 0;
   Layer *window_layer = window_get_root_layer(window);
   layer_add_child(window_layer, text_layer_get_layer(title_layer));
-  text_layer_set_text(title_layer, "Tetris");
+  text_layer_set_text(title_layer, "Tebbletris"); // such a dumb name that's crazy, but a victory for not being eligible for a cease & desist
   layer_add_child(window_layer, text_layer_get_layer(new_game_label_layer));
   layer_add_child(window_layer, text_layer_get_layer(high_score_layer));
   layer_add_child(window_layer, text_layer_get_layer(option_shadows_layer));
@@ -274,6 +305,10 @@ static void restart_after_loss() {
   text_layer_set_text(high_score_layer, strcat(high_score_str, high_score_buffer));
   layer_mark_dirty(s_title_pane_layer);
 }
+
+// ------------------------- //
+// **** BUTTON HANDLERS **** //
+// ------------------------- //
 
 static void select_click_handler(ClickRecognizerRef recognizer, void *context) {
   if (!playing && lost) {
@@ -328,7 +363,7 @@ static void select_click_handler(ClickRecognizerRef recognizer, void *context) {
     block[i] = newPos[i];
   }
   rotation = (rotation + 1) % 4;
-  layer_mark_dirty(s_left_pane_layer);
+  layer_mark_dirty(s_game_pane_layer);
 }
 
 static void up_click_handler(ClickRecognizerRef recognizer, void *context) {
@@ -353,7 +388,7 @@ static void up_click_handler(ClickRecognizerRef recognizer, void *context) {
     block[i].x -= 1;
   }
 
-  layer_mark_dirty(s_left_pane_layer);
+  layer_mark_dirty(s_game_pane_layer);
 }
 
 static void down_click_handler(ClickRecognizerRef recognizer, void *context) {
@@ -378,7 +413,7 @@ static void down_click_handler(ClickRecognizerRef recognizer, void *context) {
     block[i].x += 1;
   }
 
-  layer_mark_dirty(s_left_pane_layer);
+  layer_mark_dirty(s_game_pane_layer);
 }
 
 static void select_long_click_handler(ClickRecognizerRef recognizer, void *context) {
@@ -421,11 +456,19 @@ static void click_config_provider(void *context) {
   window_single_click_subscribe(BUTTON_ID_BACK, back_click_handler);
   window_single_click_subscribe(BUTTON_ID_SELECT, select_click_handler);
   window_long_click_subscribe(BUTTON_ID_SELECT, 500, select_long_click_handler, NULL);
-  window_long_click_subscribe(BUTTON_ID_UP, 250, up_long_click_handler, NULL);
-  window_long_click_subscribe(BUTTON_ID_DOWN, 250, down_long_click_handler, NULL);
+
+  // Deactivated Up long click and Down long click (didn't like it)
+  // window_long_click_subscribe(BUTTON_ID_UP, 250, up_long_click_handler, NULL);
+  // window_long_click_subscribe(BUTTON_ID_DOWN, 250, down_long_click_handler, NULL);
+
 }
 
-static void draw_left_pane(Layer *layer, GContext *ctx) {
+// ------------------------ //
+// **** DRAW FUNCTIONS **** //
+// ------------------------ //
+
+// Draw blocks and block shadow in game area
+static void draw_game(Layer *layer, GContext *ctx) {
   if (!playing || blockType == -1) { return; }
 
   // Fast-drop is instant, so we need to show a guide.
@@ -450,51 +493,69 @@ static void draw_left_pane(Layer *layer, GContext *ctx) {
 static void draw_bg(Layer *layer, GContext *ctx) {
   if (!playing) { return; }
 
-  // Right pane.
-  graphics_context_set_stroke_color(ctx, blueish);
+  GRect bounds = layer_get_bounds(layer);
+  int16_t boundsWidth = bounds.size.w;
+  int16_t boundsHeight = bounds.size.h;
+
+  // Color background
   graphics_context_set_fill_color(ctx, blueish);
-  GRect right_pane = GRect(80, 0, 64, 160);
-  graphics_fill_rect(ctx, right_pane, 0, GCornerNone);
-  graphics_draw_rect(ctx, right_pane);
 
-  // Bottom pane.
-  GRect bottom_pane = GRect(0, 160, 144, 8);
-  graphics_fill_rect(ctx, bottom_pane, 0, GCornerNone);
+  GRect color_bg = GRect(0, 0, boundsWidth, boundsHeight);
+  graphics_fill_rect(ctx, color_bg, 0, GCornerNone);
 
-  // Next block.
+  // Attempt at adding a Radial instead of rectangular labels to fit the Time Round aesthetic but meh, we'll see.
+  // TODO - Keeping this as a note for later
+
+  // #if defined(PBL_ROUND)
+  // graphics_context_set_fill_color(ctx, blueishish);
+  // graphics_fill_radial(ctx, bounds, GOvalScaleModeFitCircle, GRID_ORIGIN_X, DEG_TO_TRIGANGLE(45), DEG_TO_TRIGANGLE(135)); // GRID_ORIGIN_X for Radius
+  // graphics_fill_radial(ctx, bounds, GOvalScaleModeFitCircle, GRID_ORIGIN_X, DEG_TO_TRIGANGLE(235), DEG_TO_TRIGANGLE(305)); // 225-10 / 315-10 
+  // #endif
+
+  // Game BG.
+  graphics_context_set_fill_color(ctx, GColorBlack);
+  GRect game_bg = GRect(GRID_ORIGIN_X, GRID_ORIGIN_Y, GRID_PIXEL_WIDTH, GRID_PIXEL_HEIGHT);
+  graphics_fill_rect(ctx, game_bg, 0, GCornerNone);
+
+  // Grid colors from leftover blocks.
+  for (int i=0; i<GRID_BLOCK_WIDTH; i++) {
+    for (int j=0; j<GRID_BLOCK_HEIGHT; j++) {
+      if (grid_col[i][j] != 255) {
+        graphics_context_set_fill_color(ctx, col_map[grid_col[i][j]]);
+        GRect brick = GRect((i*BLOCK_SIZE) + GRID_ORIGIN_X, (j*BLOCK_SIZE) + GRID_ORIGIN_Y, BLOCK_SIZE, BLOCK_SIZE);
+        graphics_fill_rect(ctx, brick, 0, GCornerNone);
+      }
+    }
+  }
+
+  // Display preview of upcoming block
   graphics_context_set_stroke_color(ctx, GColorBlack);
   graphics_context_set_fill_color(ctx, col_map[nextBlockType]);
-  int sPosX = 108 + next_block_offset(nextBlockType);
-  int sPosY = 24;
+
+  #if defined(PBL_ROUND)
+  int sPosX = (GRID_ORIGIN_X+GRID_PIXEL_WIDTH+BLOCK_SIZE*2) + next_block_offset(nextBlockType);
+  int sPosY = GRID_ORIGIN_Y + BLOCK_SIZE*5;
+  #else
+  // for rectangular screen, center the next block display in the right pane
+  int sPosX = (GRID_ORIGIN_X+GRID_PIXEL_WIDTH+((boundsWidth-GRID_PIXEL_WIDTH)/2)) + next_block_offset(nextBlockType);
+  int sPosY = GRID_ORIGIN_Y + BLOCK_SIZE*2;
+  #endif
+
   for (int i=0; i<4; i++) {
     GRect bl = GRect(sPosX+(nextBlock[i].x*8), sPosY+(nextBlock[i].y*8), 8, 8);
     graphics_fill_rect(ctx, bl, 0, GCornerNone);
     graphics_draw_rect(ctx, bl);
   }
 
-  // Left pane BG.
-  graphics_context_set_fill_color(ctx, GColorBlack);
-  GRect left_pane = GRect(0, 0, 80, 160);
-  graphics_fill_rect(ctx, left_pane, 0, GCornerNone);
-
-  // Grid colors from leftover blocks.
-  for (int i=0; i<10; i++) {
-    for (int j=0; j<20; j++) {
-      if (grid_col[i][j] != 255) {
-        graphics_context_set_fill_color(ctx, col_map[grid_col[i][j]]);
-        GRect brick = GRect(i*8, j*8, 8, 8);
-        graphics_fill_rect(ctx, brick, 0, GCornerNone);
-      }
-    }
-  }
-
-  // Left pane grid.
+  // Game BG grid.
   graphics_context_set_stroke_color(ctx, purple);
-  for (int i=0; i<10; i++) {
-    graphics_draw_line(ctx, GPoint(i*8, 0), GPoint(i*8, 160));
+  // Draw vertical lines
+  for (int i=GRID_ORIGIN_X; i<(GRID_PIXEL_WIDTH+GRID_ORIGIN_X); i+=BLOCK_SIZE) {
+    graphics_draw_line(ctx, GPoint(i, GRID_ORIGIN_Y), GPoint(i, GRID_ORIGIN_Y + GRID_PIXEL_HEIGHT)); 
   }
-  for (int i=0; i<20; i++) {
-    graphics_draw_line(ctx, GPoint(0, i*8), GPoint(80, i*8));
+  // Draw horizontal lines
+  for (int i=GRID_ORIGIN_Y; i<(GRID_PIXEL_HEIGHT+GRID_ORIGIN_Y); i+=BLOCK_SIZE) {
+    graphics_draw_line(ctx, GPoint(GRID_ORIGIN_X, i), GPoint(GRID_ORIGIN_X+GRID_PIXEL_WIDTH, i));
   }
 }
 
@@ -530,21 +591,29 @@ static void app_focus_handler(bool focus) {
   }
 }
 
+// -------------------------- //
+// **** WINDOW FUNCTIONS **** //
+// -------------------------- //
+
 static void window_load(Window *window) {
   Layer *window_layer = window_get_root_layer(window);
   GRect bounds = layer_get_bounds(window_layer);
+  int16_t boundsWidth = bounds.size.w;
+  int16_t boundsHeight = bounds.size.h;
 
-  title_layer = text_layer_create((GRect) { .origin = { 0, 16 }, .size = { bounds.size.w, 20 } });
+  // ** TITLE WINDOW LAYERS ** //
+
+  title_layer = text_layer_create((GRect) { .origin = { 0, 16 }, .size = { boundsWidth, 20 } });
   text_layer_set_text(title_layer, "Tetris");
   text_layer_set_text_alignment(title_layer, GTextAlignmentCenter);
   layer_add_child(window_layer, text_layer_get_layer(title_layer));
 
-  new_game_label_layer = text_layer_create((GRect) { .origin = { 0, 56 }, .size = { bounds.size.w, 20 } });
+  new_game_label_layer = text_layer_create((GRect) { .origin = { 0, 56 }, .size = { boundsWidth, 20 } });
   text_layer_set_text(new_game_label_layer, "New Game");
   text_layer_set_text_alignment(new_game_label_layer, GTextAlignmentCenter);
   layer_add_child(window_layer, text_layer_get_layer(new_game_label_layer));
 
-  load_game_label_layer = text_layer_create((GRect) { .origin = { 0, 76 }, .size = { bounds.size.w, 20 } });
+  load_game_label_layer = text_layer_create((GRect) { .origin = { 0, 76 }, .size = { boundsWidth, 20 } });
   text_layer_set_text(load_game_label_layer, "Continue");
   text_layer_set_text_alignment(load_game_label_layer, GTextAlignmentCenter);
   if (can_load) {
@@ -552,7 +621,7 @@ static void window_load(Window *window) {
   }
   
   // kamotswind - Add option to toggle drop shadow. Had to move all things around on the screen.
-  option_shadows_layer = text_layer_create((GRect) { .origin = { 0, 96 }, .size = { bounds.size.w, 20 } });
+  option_shadows_layer = text_layer_create((GRect) { .origin = { 0, 96 }, .size = { boundsWidth, 20 } });
   if (persist_exists(OPTION_SHADOWS_KEY)) {
     option_shadows_buffer = persist_read_bool(OPTION_SHADOWS_KEY);
   }
@@ -568,7 +637,8 @@ static void window_load(Window *window) {
   text_layer_set_text_alignment(option_shadows_layer, GTextAlignmentCenter);
   layer_add_child(window_layer, text_layer_get_layer(option_shadows_layer));
   
-  high_score_layer = text_layer_create((GRect) { .origin = { 0, 126 }, .size = { bounds.size.w, 20 } });
+  high_score_layer = text_layer_create((GRect) { .origin = { 0, boundsHeight*0.75 }, .size = { boundsWidth, 20 } });
+
   if (persist_exists(HIGH_SCORE_KEY)) {
     itoa10(persist_read_int(HIGH_SCORE_KEY), high_score_buffer);
   }
@@ -581,29 +651,77 @@ static void window_load(Window *window) {
   text_layer_set_text_alignment(high_score_layer, GTextAlignmentCenter);
   layer_add_child(window_layer, text_layer_get_layer(high_score_layer));
 
-  s_bg_layer = layer_create(GRect(0, 0, 144, 168));
+
+  // *** PLAY WINDOW LAYERS ***
+
+  s_bg_layer = layer_create(GRect(0, 0, boundsWidth, boundsHeight));
   layer_set_update_proc(s_bg_layer, draw_bg);
 
-  s_left_pane_layer = layer_create(GRect(0, 0, 80, 168));
-  layer_set_update_proc(s_left_pane_layer, draw_left_pane);
+  s_game_pane_layer = layer_create(GRect(GRID_ORIGIN_X, GRID_ORIGIN_Y, GRID_ORIGIN_X+GRID_PIXEL_WIDTH, GRID_ORIGIN_Y+GRID_PIXEL_HEIGHT));
+  layer_set_update_proc(s_game_pane_layer, draw_game);
 
-  s_title_pane_layer = layer_create(GRect(0, 0, 144, 168));
+  s_title_pane_layer = layer_create(GRect(0, 0, boundsWidth, boundsHeight));
   layer_set_update_proc(s_title_pane_layer, draw_title_pane);
   layer_add_child(window_layer, s_title_pane_layer);
 
-  score_label_layer = text_layer_create((GRect) { .origin = { 80, 54 }, .size = { 64, 20 } });
+  score_label_layer = text_layer_create((GRect) { 
+    .origin = { 
+      GRID_ORIGIN_X+GRID_PIXEL_WIDTH, 
+      (GRID_ORIGIN_Y + BLOCK_SIZE * PBL_IF_RECT_ELSE(6, 9)) // place layer 6 or 9 blocks from top vertical coordinate of play area
+    }, 
+    .size = { 
+      boundsWidth-(GRID_ORIGIN_X+GRID_PIXEL_WIDTH), // as wide as the space to the right of the play area
+      LABEL_HEIGHT 
+    } 
+  });
   text_layer_set_text_alignment(score_label_layer, GTextAlignmentCenter);
 
-  score_layer = text_layer_create((GRect) { .origin = { 80, 84 }, .size = { 64, 20 } });
+  score_layer = text_layer_create((GRect) { 
+    .origin = { 
+      GRID_ORIGIN_X + GRID_PIXEL_WIDTH, 
+      (GRID_ORIGIN_Y + BLOCK_SIZE * PBL_IF_RECT_ELSE(7, 10) + LABEL_HEIGHT) // 7 or 10 blocks from top, below previous label
+    }, 
+    .size = { 
+      boundsWidth - (GRID_ORIGIN_X + GRID_PIXEL_WIDTH), 
+      LABEL_HEIGHT
+    } 
+  });
   text_layer_set_text_alignment(score_layer, GTextAlignmentCenter);
 
-  level_label_layer = text_layer_create((GRect) { .origin = { 80, 114 }, .size = { 64, 20 } });
+  level_label_layer = text_layer_create((GRect) { 
+    .origin = { 
+      PBL_IF_RECT_ELSE(GRID_ORIGIN_X + GRID_PIXEL_WIDTH, 0), // align to the left of play area on Round screen
+      PBL_IF_RECT_ELSE(GRID_ORIGIN_Y + BLOCK_SIZE * 8 + LABEL_HEIGHT * 2, GRID_ORIGIN_Y + BLOCK_SIZE * 7)
+    }, 
+    .size = { 
+      boundsWidth-(GRID_ORIGIN_X + GRID_PIXEL_WIDTH), 
+      LABEL_HEIGHT 
+    } 
+  });
   text_layer_set_text_alignment(level_label_layer, GTextAlignmentCenter);
   
-  level_layer = text_layer_create((GRect) { .origin = { 80, 144 }, .size = { 64, 20 } });
+  level_layer = text_layer_create((GRect) { 
+    .origin = { 
+      PBL_IF_RECT_ELSE(GRID_ORIGIN_X + GRID_PIXEL_WIDTH, 0), 
+      PBL_IF_RECT_ELSE(GRID_ORIGIN_Y + BLOCK_SIZE * 9 + LABEL_HEIGHT * 3, GRID_ORIGIN_Y + BLOCK_SIZE * 8 + LABEL_HEIGHT) 
+    }, 
+    .size = { 
+      boundsWidth - (GRID_ORIGIN_X + GRID_PIXEL_WIDTH), 
+      LABEL_HEIGHT 
+    } 
+  });
   text_layer_set_text_alignment(level_layer, GTextAlignmentCenter);
 
-  paused_label_layer = text_layer_create((GRect) { .origin = { 0, 72 }, .size = { 80, 20 } });
+  paused_label_layer = text_layer_create((GRect) { 
+    .origin = { 
+      GRID_ORIGIN_X, 
+      GRID_ORIGIN_Y + (GRID_PIXEL_HEIGHT/2) - LABEL_HEIGHT // place in middle of play area
+    }, 
+    .size = { 
+      GRID_PIXEL_WIDTH, 
+      LABEL_HEIGHT 
+    } 
+  });
   text_layer_set_text(paused_label_layer, "Paused");
   text_layer_set_text_alignment(paused_label_layer, GTextAlignmentCenter);
 
@@ -624,7 +742,7 @@ static void window_unload(Window *window) {
   text_layer_destroy(option_shadows_layer);
   text_layer_destroy(high_score_layer);
   layer_destroy(s_bg_layer);
-  layer_destroy(s_left_pane_layer);
+  layer_destroy(s_game_pane_layer);
   layer_destroy(s_title_pane_layer);
   gpath_destroy(selector_path);
 }
